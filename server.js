2c228714092c39bc797001ec858470de3b1a614b
charmy1977 <142159795+charmy1977@users.noreply.github.com>
2026-04-08 07:28:17 +0530

    chore: set up initial folder structure and server configuration

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+// const { Listing } = require("./models/listing");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const Database_url = process.env.DATABASE_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(Database_url);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
