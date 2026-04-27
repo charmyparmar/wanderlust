f34ded0035a149b39efa6526c7126304b24dcd0f
charmy1977 <142159795+charmy1977@users.noreply.github.com>
2026-04-27 23:20:44 +0530

    feat: add full CRUD operations for listings

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const initData = require('./data');
+const { Listing } = require('../models/listing');
+
+require('dotenv').config();
+
+const database_url = process.env.MONGO_URL;
+
+main()
+  .then(() => {
+    console.log('connected to DB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(database_url);
+}
+
+const initDB = async () => {
+  // await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log('data was initialized');
+};
+
+initDB();
