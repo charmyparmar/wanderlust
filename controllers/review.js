c7cab06aebe4411fe1b03924ba4a24f756d2f352
charmy1977 <142159795+charmy1977@users.noreply.github.com>
2026-06-01 08:29:53 +0530

    feat: add isLoggedIn, isOwner, and isReviewAuthor resource guards to Auth Module

--- controllers/review.js
+++ controllers/review.js
@@ -6,6 +6,7 @@ const { Listing } = require('../models/listing');
 module.exports.createReview = async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   const newReview = new Review(req.body.review);
+  newReview.author = req.user._id;
 
   await newReview.save();
 
--- routes/review.js
+++ routes/review.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const ExpressError = require('../utils/ExpressError');
 const { reviewSchema } = require('../utils/validator/schema');
-
 const reviewController = require('../controllers/review');
-const { Listing } = require('../models/listing');
+const { isLoggedIn, isReviewAuthor } = require('../middleware');
 
 const validateReview = (req, res, next) => {
   let { error } = reviewSchema.validate(req.body);
@@ -17,8 +16,8 @@ const validateReview = (req, res, next) => {
   }
 };
 
-router.post('/', validateReview, reviewController.createReview);
+router.post('/', isLoggedIn, validateReview, reviewController.createReview);
 
-router.delete('/:reviewId', reviewController.deleteReview);
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, reviewController.deleteReview);
 
 module.exports = router;
